1740fd3d9617b6c521790014f9a01b1cac39974c
Patrick Almeida <patricksalmeida@gmail.com>
2022-09-05 20:36:32 -0300

    initial commit

---
+++ projects/mylibrary/src/lib/mylibrary.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ml-mylibrary',
+  template: `
+    <p>
+      mylibrary works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MylibraryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
